7135bbf26ef95bafd8ec3e5e905038e5c0f34dc7
rathnamkoninti <rathnamkoninti@gmail.com>
2021-07-16 15:55:35 +0530

    commit two2155444s45145

--- shopping/urls.py
+++ shopping/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('', include('testapp.urls')),
 
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),\
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
--- testapp/urls.py
+++ testapp/urls.py
@@ -69,5 +69,4 @@ urlpatterns = [
 
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
-] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT),\
-              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
