0ec2b7707e2c5d5d11934784ce551f46ef09fd7b
rathnamkoninti <rathnamkoninti@gmail.com>
2021-07-16 15:17:05 +0530

    commit two255444s

--- shopping/settings.py
+++ shopping/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-(03nh#uy=sak36z+_3!br1n4pme&1+g8tc&$%8z0t_n##(s#uk
 # SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ["rktesting111.herokuapp.com","127.0.0.1"]
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rktesting111.herokuapp.com', '*', '127.0.1.1']
@@ -122,17 +122,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = "/media/"
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
