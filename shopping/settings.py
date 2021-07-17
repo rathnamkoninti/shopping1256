e51a485dc704c8d1bfdd73f8dc33d424dddc43af
rathnamkoninti <rathnamkoninti@gmail.com>
2021-07-17 13:42:43 +0530

    commit 5521152h12h

--- shopping/settings.py
+++ shopping/settings.py
@@ -156,4 +156,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rktesting1502@gmail.com'
-EMAIL_HOST_PASSWORD = 'vennela@137'
+EMAIL_HOST_PASSWORD = 'Vennela@137'
