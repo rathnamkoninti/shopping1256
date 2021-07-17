387dfd0b04192a7dbf8d3e34929dde7be2ba3fe4
rathnamkoninti <rathnamkoninti@gmail.com>
2021-07-17 13:15:06 +0530

    commit 5521152h

--- shopping/settings.py
+++ shopping/settings.py
@@ -148,3 +148,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "rktesting1502@gmail.com"
+EMAIL_HOST_PASSWORD = "rktesting@15022"
