3499d98edde0955f5e6f6dcdeff0b8c570313cab
rathnamkoninti <rathnamkoninti@gmail.com>
2021-07-19 13:19:40 +0530

    commit 5521hhl1h

--- shopping/urls.py
+++ shopping/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testapp.urls')),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
